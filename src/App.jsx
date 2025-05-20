af3c68ef669b6ca6236d5921346146743ad65190
Sara <sarailic431@gmail.com>
2025-05-20 19:42:31 +0200

    fetching data by ingredients

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,13 @@ import CoctailItem from "./CoctailItem";
 function App() {
   const [coctails, setCoctails] = useState(null);
   const [ingredient, setIngredient] = useState("Tequila");
+  const [query, setQuery] = useState("");
 
-  // const BASE_URL = `https://thecocktaildb.com/api/json/v1/1/filter.php?i=${ingredient}`;
-  // const BASE_URL = `https://thecocktaildb.com/api/json/v1/1/filter.php?i=Vodka`;
   useEffect(() => {
     const fetchPost = async () => {
-      // const response = await fetch(`${BASE_URL}`);
       const response = await fetch(
         `https://thecocktaildb.com/api/json/v1/1/filter.php?i=${ingredient}`
+        // ` https://thecocktaildb.com/api/json/v1/1/search.php?s=margarita`
       );
       const posts = await response.json();
       console.log(posts.drinks);
@@ -31,7 +30,21 @@ function App() {
         <button onClick={() => setIngredient("Vodka")}>Vodka</button>
         <button onClick={() => setIngredient("Rum")}>Rum</button>
         <button onClick={() => setIngredient("Tequila")}>Tequila</button>
+        <button onClick={() => setIngredient("Wine")}>Wine</button>
+        <button onClick={() => setIngredient("Whiskey")}>Whiskey</button>
+        <button onClick={() => setIngredient("Aperol")}>Aperol</button>
       </div>
+
+      <form>
+        <label>Search by name:</label>
+        <input
+          type="text"
+          className="input-name"
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        <button className="hidden"></button>
+      </form>
 
       <div className="coctails-list">
         {coctails &&
