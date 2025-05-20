bf77e0fd2e9141d3e78d42717a8a305930742a26
Sara <sarailic431@gmail.com>
2025-05-20 12:31:52 +0200

    ....

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,56 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <h1>Our Coctails</h1>
+
+      <div className="coctails-list">
+        <article className="coctail-item">
+          <span>
+            {" "}
+            <img src="./cosmopolitan.jpg" className="coctail-img" />{" "}
+          </span>
+          <h5>Cosmopolitan</h5>
+        </article>
+        <article className="coctail-item">
+          <span>
+            {" "}
+            <img src="./cosmopolitan.jpg" className="coctail-img" />{" "}
+          </span>
+          <h5>Cosmopolitan</h5>
+        </article>
+        <article className="coctail-item">
+          <span>
+            {" "}
+            <img src="./cosmopolitan.jpg" className="coctail-img" />{" "}
+          </span>
+          <h5>Cosmopolitan</h5>
+        </article>
+        <article className="coctail-item">
+          <span>
+            {" "}
+            <img src="./cosmopolitan.jpg" className="coctail-img" />{" "}
+          </span>
+          <h5>Cosmopolitan</h5>
+        </article>
+        <article className="coctail-item">
+          <span>
+            {" "}
+            <img src="./cosmopolitan.jpg" className="coctail-img" />{" "}
+          </span>
+          <h5>Cosmopolitan</h5>
+        </article>
+        <article className="coctail-item">
+          <span>
+            {" "}
+            <img src="./cosmopolitan.jpg" className="coctail-img" />{" "}
+          </span>
+          <h5>Cosmopolitan</h5>
+        </article>
+      </div>
+    </>
+  );
 }
 
 export default App;
