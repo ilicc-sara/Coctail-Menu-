64411ac8e63047ddcf4b6807d30a57116397cda6
Sara <sarailic431@gmail.com>
2025-06-22 23:52:10 +0200

    click fetch by name instead of id

--- src/CoctailItem.jsx
+++ src/CoctailItem.jsx
@@ -2,12 +2,14 @@ import React from "react";
 
 function CoctailItem(props) {
   const { image, name, id } = props;
-  // console.log(id);
+  console.log(id);
 
-  function showIngredients(id) {
+  function showIngredients(name) {
     const fetchPost = async () => {
       const response = await fetch(
-        `https://thecocktaildb.com/api/json/v1/1/lookup.php?iid=${id}`
+        // `https://thecocktaildb.com/api/json/v1/1/lookup.php?iid=${id}`
+        // `https://thecocktaildb.com/api/json/v1/1/lookup.php?s=${name}`
+        `https://thecocktaildb.com/api/json/v1/1/search.php?s=${name}`
       );
       const posts = await response.json();
       console.log(posts);
@@ -16,7 +18,7 @@ function CoctailItem(props) {
     fetchPost();
   }
   return (
-    <article className="coctail-item" onClick={() => showIngredients(id)}>
+    <article className="coctail-item" onClick={() => showIngredients(name)}>
       <span>
         {" "}
         <img src={image} className="coctail-img" />{" "}
